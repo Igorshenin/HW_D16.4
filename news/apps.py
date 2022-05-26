add871f7901b76dbfb66e2e68896ac7ad6acafc0
Igorshenin <92224065+Igorshenin@users.noreply.github.com>
2022-05-26 22:43:50 +0400

    Initial commit

---
+++ news/apps.py
@@ -0,0 +1,23 @@
+from django.apps import AppConfig
+import redis
+
+
+class NewsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'news'
+
+    def ready(self):
+        import news.signals
+ 
+class PostConfig(AppConfig):
+    name = 'news1'
+ 
+    # нам надо переопределить метод ready, чтобы при готовности нашего приложения импортировался модуль со всеми функциями обработчиками
+    def ready(self):
+        import news.signals
+
+red = redis.Redis(
+    host ='redis-10094.c296.ap-southeast-2-1.ec2.cloud.redislabs.com',
+    port = 10094,
+    password='Mvd5hnt5RHZ1YDif1KbkGOJfiL6GIC13'
+)
